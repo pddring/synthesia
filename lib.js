6f0b6d09d73667948d6b841300ed0aa76635c02b
Pete Dring <dringp@fulford.york.sch.uk>
2023-03-06 20:12:55 +0000

    Initial commit

---
+++ lib.js
@@ -0,0 +1,57 @@
+function init() {
+    eLog = $('.status');
+    WebMidi
+    .enable()
+    .then(onEnabled)
+    .catch(err => log("Error:" + err));
+}
+var eLog;
+
+var colours = {
+    "C":"#d5e0f9",
+    "C#":"#ed7a30",
+    "D":"#070670",
+    "D#":"#f2a93b",
+    "E":"#f6d548",
+    "F":"#bdda4a",
+    "F#":"#cb4274",
+    "G":"#f3c151",
+    "G#":"#72ab48",
+    "A":"#9b2d21",
+    "A#":"#e09265",
+    "B":"#c4e493",
+}
+
+function log(msg) {
+    console.log(msg);
+    eLog.text(msg);
+}
+
+// Function triggered when WEBMIDI.js is ready
+function onEnabled() {
+
+    // Display available MIDI input devices
+    if (WebMidi.inputs.length < 1) {
+        log("No device detected.");
+    } else {
+      WebMidi.inputs.forEach((device, index) => {
+        log(`Connected to MIDI device: ${device.name}`);
+
+        device.channels[1].addListener("noteon", e => {
+            let n = e.note.name;
+            if(e.note.accidental)
+                n += e.note.accidental;
+            log(`Note on: ${n}`);
+            $('body').css('background-color', colours[n]);
+        });
+        
+        device.channels[1].addListener("noteoff", e => {
+            log("Note off");
+            $('body').css('background-color', 'white');
+        });
+      });
+    }
+
+  }
+
+$(init);
